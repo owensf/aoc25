460940e57d3890d21347b032c33757ce93469a57
Fred <fredward1435@gmail.com>
2025-04-01 20:45:35 -0500

    part 1 solved

--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,24 @@
-def day1_placeholder()
-    return
+def parse(filename):
+    leftlist = []
+    rightlist = []
+    with open(filename, 'r') as f:
+        while line := f.readline():
+            newline = line.split()
+            leftlist.append(newline[0])
+            rightlist.append(newline[1])
+            #print(newline)
+    leftlist.sort()
+    rightlist.sort()
+    return leftlist, rightlist
+
+def find_sum(leftlist, rightlist):
+    sum = 0
+    for left, right in zip(leftlist, rightlist):
+        sum += abs(int(left) - int(right))
+    return sum
 
 
 if __name__ == '__main__':
-    print(day1_placeholder())
+    leftlist, rightlist = parse("day1input.txt")
+    sum = find_sum(leftlist, rightlist)
+    print(sum)
